06ae82e12a32732216e00d7e729d682a236bef1c
Cologler <skyoflw@gmail.com>
2018-08-18 08:44:58 +0800

    update to 0.2.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     # auto generated:
     name='anyioc',
-    version='0.2.1',
+    version='0.2.2',
     description='',
     keywords=[],
     author='Cologler',
