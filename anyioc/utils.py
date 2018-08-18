14e220744b904f13e900a9a30a8fe44b136fddc3
Cologler <skyoflw@gmail.com>
2018-08-18 12:54:40 +0800

    add auto_enter()

--- anyioc/utils.py
+++ anyioc/utils.py
@@ -8,13 +8,13 @@
 import sys
 from inspect import signature, Parameter
 
-from .err import ServiceNotFoundError
-
 def auto_inject(func):
     '''
     wrap the func and auto inject by parameter name.
 
     var keyword parameter and var positional parameter will not be inject.
+
+    return the new func with signature: `(ioc) => any`
     '''
     sign = signature(func)
     names = [p.name for p in sign.parameters.values() if p.kind in (
@@ -26,6 +26,18 @@ def auto_inject(func):
         for name in names:
             kwargs[name] = ioc[name]
         return func(**kwargs)
+    return new_func
+
+def auto_enter(func):
+    '''
+    auto enter the context manager when it created.
+
+    the signature of func should be `(ioc) => any`.
+    '''
+    def new_func(ioc):
+        item = func(ioc)
+        ioc.enter(item)
+        return item
     return new_func
 
 def dispose_at_exit(provider):
