91485d506683e3b893539f031a99e30ea485b618
Cologler <skyoflw@gmail.com>
2018-08-19 15:40:46 +0800

    update to v0.3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     # auto generated:
     name='anyioc',
-    version='0.3.0',
+    version='0.3.1',
     description='',
     keywords=[],
     author='Cologler',
