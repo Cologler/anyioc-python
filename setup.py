3d7bcebd1fc4b3779696d3368526e8fd7566bc2c
Cologler <skyoflw@gmail.com>
2018-12-06 10:11:32 +0800

    update to v0.4.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     # auto generated:
     name='anyioc',
-    version='0.4.1',
+    version='0.4.2',
     description='',
     keywords=[],
     author='Cologler',
