8dd7e48ae74232a6d77f867f5a3deb5d0b866de9
Cologler <skyoflw@gmail.com>
2019-06-09 06:51:13 +0800

    remove unused

--- tests/assert_utils.py
+++ tests/assert_utils.py
@@ -9,37 +9,6 @@ import itertools
 
 from anyioc import ServiceProvider
 
-def assert_singleton(ioc: ServiceProvider, key, *other_keys):
-    root_value = ioc[key]
-    assert ioc[key] is root_value
-    for alias in other_keys:
-        assert ioc[alias] is root_value
-    with ioc.scope() as scoped:
-        scoped_value = scoped[key]
-        assert scoped_value is root_value
-        for alias in other_keys:
-            assert scoped[alias] is root_value
-    return True
-
-def assert_scoped(ioc: ServiceProvider, key, *other_keys):
-    root_value = ioc[key]
-    assert ioc[key] is root_value
-    for alias in other_keys:
-        assert ioc[alias] is root_value
-    with ioc.scope() as scoped:
-        scoped_value = scoped[key]
-        assert scoped_value is not root_value
-        for alias in other_keys:
-            assert scoped[alias] is scoped_value
-            assert scoped[alias] is not root_value
-    return True
-
-def assert_transient(ioc: ServiceProvider, key, *other_keys):
-    assert ioc[key] is not ioc[key]
-    with ioc.scope() as scoped:
-        assert scoped[key] is not ioc[key]
-    return True
-
 def assert_value_singleton(provider: ServiceProvider, key):
     scoped_1 = provider.scope()
     scoped_2 = provider.scope()
