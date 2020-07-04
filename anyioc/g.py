48cb236663b7aae28822c38ee4c42db0469f2f45
Cologler <skyoflw@gmail.com>
2020-07-04 16:22:12 +0800

    fixes: module scoped provider should be root provider

--- anyioc/g.py
+++ anyioc/g.py
@@ -8,6 +8,7 @@
 import importlib
 import functools
 import inspect
+import threading
 
 from .ioc import ServiceProvider
 from .utils import inject_by_name, dispose_at_exit
@@ -17,35 +18,28 @@ dispose_at_exit(ioc)
 
 # scoped global ioc
 
-def _make_module_scoped_provider():
-    from .ioc_resolver import IServiceInfoResolver
-    from .ioc_service_info import ValueServiceInfo, IServiceInfo
-    from .symbols import Symbols
+_module_scoped_providers = {}
+_module_scoped_lock = threading.RLock()
 
-    class ServiceProviderServiceInfoResolver(IServiceInfoResolver):
-        def get(self, provider: ServiceProvider, key) -> IServiceInfo:
-            new_provider = ServiceProvider()
-            provider.enter(new_provider)
-
-            try:
-                init_ioc = importlib.import_module(key + '.init_ioc')
-            except ImportError:
-                init_ioc = None
-            if init_ioc is not None:
-                conf_ioc = getattr(init_ioc, 'conf_ioc', None)
-                if conf_ioc is not None:
-                    conf_ioc(new_provider)
-
-            return ValueServiceInfo(new_provider)
-
-    provider = ServiceProvider()
-    provider[Symbols.missing_resolver].append(
-        ServiceProviderServiceInfoResolver().cache(sync=True)
-    )
-    dispose_at_exit(provider)
+def _get_module_provider(module_name: str):
+    'get or create module provider'
+    provider = _module_scoped_providers.get(module_name)
+    if provider is None:
+        with _module_scoped_lock:
+            provider = _module_scoped_providers.get(module_name)
+            if provider is None:
+                provider = ServiceProvider()
+                dispose_at_exit(provider)
+                try:
+                    init_ioc = importlib.import_module(module_name + '.init_ioc')
+                except ImportError:
+                    init_ioc = None
+                if init_ioc is not None:
+                    conf_ioc = getattr(init_ioc, 'conf_ioc', None)
+                    if conf_ioc is not None:
+                        conf_ioc(provider)
+                _module_scoped_providers[module_name] = provider
     return provider
-
-_module_scoped_providers = _make_module_scoped_provider()
 
 def get_module_provider(module_name: str=None) -> ServiceProvider:
     '''
@@ -67,7 +61,7 @@ def get_module_provider(module_name: str=None) -> ServiceProvider:
     if not isinstance(module_name, str):
         raise TypeError
 
-    return _module_scoped_providers[module_name]
+    return _get_module_provider(module_name)
 
 def get_namespace_provider(namespace: str=None) -> ServiceProvider:
     '''
@@ -86,4 +80,4 @@ def get_namespace_provider(namespace: str=None) -> ServiceProvider:
         raise TypeError
 
     namespace = namespace.partition('.')[0]
-    return _module_scoped_providers[namespace]
+    return _get_module_provider(namespace)
